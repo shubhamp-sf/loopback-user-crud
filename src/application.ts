a650f8fb8ca9042b76ae73ea5b08be726542fac0
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-08-23 23:22:04 +0530

    feat(origin-check): add sequence to only allow specific origins mentioned in env

--- src/application.ts
+++ src/application.ts
@@ -7,7 +7,10 @@ import {
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
+import dotenv from 'dotenv';
+import dotEnvExtended from 'dotenv-extended';
 import path from 'path';
+import {OriginCheckSequence} from './origin-check.sequence';
 import {RequestLoggerSequence} from './request-logger.sequence';
 import {MySequence} from './sequence';
 
@@ -17,11 +20,17 @@ export class UserCrudApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
+    dotenv.config();
+    dotEnvExtended.load({
+      schema: '.env.example',
+    });
+    console.log(process.env.ALLOWED_ORIGINS);
     super(options);
 
     // Set up the custom sequence
     this.sequence(MySequence);
     this.sequence(RequestLoggerSequence);
+    this.sequence(OriginCheckSequence);
 
     // Set up default home page
     this.static('/', path.join(__dirname, '../public'));
